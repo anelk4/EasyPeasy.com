64b0c935af5026b39dca4d7198d9ebf7afaebee9
Bakyt Zhansaya <zansaabakyt364@gmail.com>
2023-04-15 02:22:37 +0600

    commit byl zherde script.js file zhasalyndt login,update,degister html kod zhazyldy urls.py men views.py url adresterin zhazyldy

--- MainFolder/applacation1/urls.py
+++ MainFolder/applacation1/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     path('courses/',views.courses, name='courses'),
     path('',views.home, name='home'),
-    path('profile/',views.profile, name='profile')
+    path('profile/',views.profile, name='profile'),
+    path('login/',views.login, name='login'),
+    path('update/',views.update, name='update'),
+    path('degister/',views.degister, name='degister')
 ]
